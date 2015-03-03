0e5b9aa3af5f36a9d4cdfef4a1734f0c38ac28ec
Josh Butts <josh@joshbutts.com>
2015-03-02 22:04:03 -0600

    fix permissions

--- marathon/marathon.sh
+++ marathon/marathon.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -L -X POST -H "Content-Type: application/json" http://10.132.83.147:8080/v2/apps -d@$1
+curl -L -X POST -H "Content-Type: application/json" http://10.132.83.147:8080/v2/apps -d@myapp.json
